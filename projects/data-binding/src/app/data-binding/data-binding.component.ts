b2633975a4bb7422caf0129cb5c9d608d19a19c9
leandroltf10 <leandroltf10@gmail.com>
2022-04-15 10:09:26 -0300

    event binding

--- projects/data-binding/src/app/data-binding/data-binding.component.ts
+++ projects/data-binding/src/app/data-binding/data-binding.component.ts
@@ -9,6 +9,9 @@ export class DataBindingComponent implements OnInit {
 
   url: string = 'http://loiane.com';
   urlImagem: string = 'http://lorempixel.com.br/100/100/?1';
+  valorAtual: string = '';
+  valorSalvo: string | undefined = '';
+  isMouseOver: boolean = false;
 
   constructor() { }
 
@@ -21,6 +24,22 @@ export class DataBindingComponent implements OnInit {
 
   getBoolean(): boolean {
     return true;
+  }
+
+  botaoClicado() {
+    alert('botão clicado');
+  }
+
+  onKeyUp(valor: string) {
+    this.valorAtual = valor;
+  }
+
+  salvarValor(valor: string) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
 }
